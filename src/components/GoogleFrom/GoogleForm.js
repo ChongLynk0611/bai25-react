e8243c4518d91eb37245cf33c95eae542008abca
ChongLynk <chonglynk@pop-os.localdomain>
2020-10-08 21:34:04 +0700

    chuyen class component sang function stateless

--- src/components/GoogleFrom/GoogleForm.js
+++ src/components/GoogleFrom/GoogleForm.js
@@ -2,27 +2,18 @@ import React,{Component} from 'react';
 import './GoogleForm.css';
 import logo from '../../public/img/logo.png';
 
-class GoogleForm extends Component{
-    constructor(){
-        super();
-        this.inputElement = React.createRef();
-
-    }
-    componentDidMount(){
-        this.inputElement.current.focus();
-    }
-    render(){
-        return(
-            <div className="GoogleForm">
-                <img src={logo}></img>
-                <input 
-                    ref={this.inputElement}
-                    placeholder="Type searching..."
-                ></input>
-                <button>Tìm trên google</button>
-            </div>
-        );
-    }
+function GoogleForm(props){
+   
+    return(
+        <div className="GoogleForm">
+            <img src={logo}></img>
+            <input 
+                placeholder="Type searching..."
+            ></input>
+            <button>Tìm trên google</button>
+        </div>
+    );
+    
 }
 
 export default GoogleForm;
